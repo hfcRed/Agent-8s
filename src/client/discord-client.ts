9f3014eba4e86a96acc5d629b4686ba74871c953
hfcRed <hfcred@gmx.net>
2025-11-08 09:58:43 +0100

    Dont exit on rejection

--- src/client/discord-client.ts
+++ src/client/discord-client.ts
@@ -59,12 +59,10 @@ export function setupErrorHandlers(client: Client) {
 
 	process.on('uncaughtException', (error) => {
 		console.error('Uncaught Exception:', error);
-		process.exit(1);
 	});
 
 	process.on('unhandledRejection', (reason, promise) => {
 		console.error('Unhandled Rejection at:', promise, 'reason:', reason);
-		process.exit(1);
 	});
 }
 
